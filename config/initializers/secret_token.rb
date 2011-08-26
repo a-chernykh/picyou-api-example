6d190bf0b3148c244265700ff16359b5b6582413
Andrey Chernih <andrey.chernih@gmail.com>
2011-08-26 16:02:06 +0400

    dummy rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicyouApiExample::Application.config.secret_token = 'df13e1e5ab3879096d2849d933f7fb5057e4913bef07ffcbf1e15e27943143862ed03d62663d6522c5598ad817c99356d332bd1270f3c0dd7bf392c861b4a771'
